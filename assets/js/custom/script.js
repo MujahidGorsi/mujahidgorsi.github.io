cc24d9c71e6246282c56941d215116450889db22
Usman Khan <chaudhry_uk_gorsi@hotmail.com>
2025-03-07 03:10:00 +0500

    Showing only the selected category.

--- assets/js/custom/script.js
+++ assets/js/custom/script.js
@@ -1,34 +1,42 @@
-var sectionIdArray = ["all", "logo-design", "post-design", "packaging-design", "3d-product-design", "banner-design", "print-design", "illustration", "flyer-design"];
+var headerItems = [
+    { id: 0, linkId: "all", name: "All" },
+    { id: 1, linkId: "logo-design", name: "Logo Design" },
+    { id: 2, linkId: "post-design", name: "post Design" },
+    { id: 3, linkId: "packaging-design", name: "Packaging Design" },
+    { id: 4, linkId: "3d-product-design", name: "3d Product Design" },
+    { id: 5, linkId: "banner-design", name: "Banner Design" },
+    { id: 6, linkId: "print-design", name: "Print Pesign" },
+    { id: 7, linkId: "illustration", name: "Illustration" },
+    { id: 8, linkId: "flyer-design", name: "Flyer Design" },
+];
 
-(async () => {
-
-    $.fn.randomize = function (selector) {
-        var $elems = selector ? $(this).find(selector) : $(this).children();
-        for (var i = $elems.length; i >= 0; i--) {
-            $(this).append($elems[Math.random() * i | 0]);
-        }
-        return this;
+$.fn.randomize = function (selector) {
+    var $elems = selector ? $(this).find(selector) : $(this).children();
+    for (var i = $elems.length; i >= 0; i--) {
+        $(this).append($elems[Math.random() * i | 0]);
     }
+    return this;
+}
 
-    $.getJSON('../../assets/data/data.json', (data) => {
-        sectionIdArray.forEach(x => {
-            $('#designs-section').append(`
-                <section class="home-tow-gallery pt-80 pb-20" id=${x}>
-                   <div class="row" id="design-list"></div>
-                </section>
-            `);
-        });
+$.each(headerItems, (i, x) => {
+    $('.nav-items').append(`<li><a href="#${x.linkId}" onclick="showHideSection(${i}, '${x.linkId}')">${x.name}</a></li>`);
+    $('#designs-section').append(`
+        <section class="home-tow-gallery pt-80 pb-20 d-none" id=${x.linkId}>
+           <div class="row" id="design-list"></div>
+        </section>
+    `);
+});
 
-        $.each(data.portfolioDesign, (i, design) => {
-            if (design.linkId !== "all") {
-                this.renderDesigns(i, design.linkId, design);
-            }
-            this.renderDesigns(i, "all", design);
-        });
-
-        $('#all #design-list').randomize();
+$.getJSON('../../assets/data/data.json', (data) => {
+    $.each(data.portfolioDesign, (i, design) => {
+        if (design.linkId !== "all") {
+            this.renderDesigns(i, design.linkId, design);
+        }
+        this.renderDesigns(i, "all", design);
     });
-})();
+    showHideSection(0, 'all');
+    $('#all #design-list').randomize();
+});
 
 renderDesigns = (id, sectionId, design) => {
     $(`#${sectionId} #design-list`).append(`
@@ -44,6 +52,15 @@ renderDesigns = (id, sectionId, design) => {
             </a>
         </div>
     `);
+}
+
+showHideSection = (id, linkId) => {
+    $.each(headerItems, (i, item) => {
+        if (i != id) {
+            $(`#designs-section, #${item.linkId}`).addClass('d-none');
+        }
+    });
+    $(`#designs-section, #${linkId}`).removeClass('d-none');
 }
 
 getDesignDetails = (id) => {
